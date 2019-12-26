a34317b7a1b825104d7ef6e09bb85a8b32004cb0
amitramesh <amit.ramesh.sharma@gmail.com>
2019-12-26 18:05:09 +0530

    Add Database Migration For Code First Approach

---
+++ SB.Product.API/SB.Product.Api.Database/ProductContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace SB.Product.Api.Database
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions options) :base(options)
+        {
+
+        }
+        public DbSet<Models.Product> Products { get; set; }
+    }
+}
